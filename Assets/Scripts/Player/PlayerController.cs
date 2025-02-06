2a349368c8f0ab7ad3f7fab1bf1f83edf750aa4c
MisterDodik <drazenpetrovic56@gmail.com>
2025-02-07 00:49:14 +0100

    Added Crosshair

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,29 +1,49 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Windows;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     Rigidbody rb;
 
+    //Movement
+    Vector2 m_moveInput;
     public float m_speed = 50f;
-
     public float m_groundDrag = 10;
 
+    //Camera rotation
     public Transform cameraTransform;
-    Vector2 m_moveInput;
+
+    //Crosshair
+    public LayerMask layerMask;
+    public GameObject crosshairGO;
+    RawImage crosshairImage;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.linearDamping= m_groundDrag;
+
+        crosshairImage = crosshairGO.GetComponent<RawImage>();
     }
+
+    private void Update()
+    {
+        if(Physics.Raycast(cameraTransform.position, cameraTransform.forward, 2, layerMask))
+        {
+            crosshairImage.color = Color.black;
+        }
+        else
+        {
+            crosshairImage.color = Color.white;
+        }
+    }
+
 
     private void FixedUpdate()
     {
         Vector3 move = cameraTransform.forward * m_moveInput.y + cameraTransform.right * m_moveInput.x;
         move.y = 0;
-        // rb.linearVelocity = new Vector3(input.x, rb.linearVelocity.y, input.y)*speed*Time.deltaTime;
         rb.AddForce(move.normalized * m_speed * Time.deltaTime, ForceMode.VelocityChange);
     }
     public void onMove(Vector2 input)
