f5df721aec470e768eb1f5c4138aeb15c66ca97a
codebender <khoa28122005@gmail.com>
2025-02-14 22:44:36 +0700

    Add Basic Money

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public event EventHandler<OnMoneyChangedEventArgs> OnMoneyChanged;
+    public class OnMoneyChangedEventArgs : EventArgs
+    {
+        public float money;
+    }
+    private float currentMoney;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            ChangeMoney(+500);
+        }
+    }
+    public void ChangeMoney(float amount)
+    {
+        currentMoney += amount;
+        OnMoneyChanged?.Invoke(this, new OnMoneyChangedEventArgs()
+        {
+            money = currentMoney,
+        });
+    }
+}
--- Assets/Scripts/UI/CanvasGameplay.cs
+++ Assets/Scripts/UI/CanvasGameplay.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,9 +6,20 @@ public class CanvasGameplay : UICanvas
 {
     [SerializeField] private Slider suspiciousSlider;
     [SerializeField] private Slider staminaSlider;
+    [SerializeField] private Player player;
+    [SerializeField] private TextMeshProUGUI moneyText;
 
     public Slider GetStaminaSlider()
     {
         return staminaSlider;
+    }
+    private void Start()
+    {
+        player.OnMoneyChanged += Player_OnMoneyChanged;
+    }
+
+    private void Player_OnMoneyChanged(object sender, Player.OnMoneyChangedEventArgs e)
+    {
+        moneyText.text = $"{e.money:N0}$";
     }
 }
